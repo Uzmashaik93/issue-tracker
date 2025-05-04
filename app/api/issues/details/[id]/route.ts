375173597efc49c18ea7372f7dba962451795934
uzma shaik <uzma97910@gmail.com>
2025-05-04 22:46:58 +0200

    feat: implement PUT and DELETE endpoints for issue management

--- app/api/issues/details/[id]/route.ts
+++ app/api/issues/details/[id]/route.ts
@@ -16,3 +16,39 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
     }
     return NextResponse.json(response, { status: 200 });
 }
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    const id = params.id;
+    if (!id) {
+        return NextResponse.json({ error: "Issue ID is required" }, { status: 400 });
+    }
+    const body = await request.json();
+    const response = await prisma.issue.update({
+        where: {
+            id: Number(id)
+        }, data: {
+            title: body.title,
+            description: body.description
+        }
+    })
+    if (!response) {
+        return NextResponse.json({ message: "Issue not found", status: 404 });
+    }
+    return NextResponse.json(response, { status: 200 });
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const id = params.id;
+    if (!id) {
+        return NextResponse.json({ error: "Issue ID is required" }, { status: 400 });
+    }
+    const response = await prisma.issue.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+    if (!response) {
+        return NextResponse.json({ message: "Issue not found", status: 404 });
+    }
+    return NextResponse.json(response, { status: 200 });
+}
